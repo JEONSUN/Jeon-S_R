1e2019322f80324acde5f01c83ae7f6d05052b41
JEONSUN <dnwn3311@naver.com>
2020-06-06 20:29:34 +0900

    200606

--- R 프로그래밍 기초/ggplot(2) - 1.R
+++ R 프로그래밍 기초/ggplot(2) - 1.R
@@ -31,3 +31,46 @@ ggplot(data = mpg) +
 # mapping - 변수와의 연결을 의미 -변수 이름은 blue라는 값을 갖는 변수 생성 - 
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, color = "blue"))
+
+
+##################################################################################
+library(tidyverse)
+ggplot(data=mpg) + 
+  geom_point(mapping=aes(x=displ, y=hwy)) + 
+  facet_wrap(~ class)  # class 별로 facet
+
+# class변수의 2seater를 제거하고 그래프 출력
+mpg %>% filter(class != "2seater") %>%
+  ggplot() + 
+  geom_point(mapping=aes(x=displ, y=hwy)) + 
+  facet_wrap(~ class)
+
+# 패널 배치 조절
+# facet_wrap
+pp <- mpg %>%
+  filter(class != "2seater") %>%
+  ggplot() +
+  geom_point(mapping=aes(x=displ, y=hwy))
+pp + facet_wrap(~class, ncol=2) # 행별로 정렬
+pp + facet_wrap(~class, ncol=2, dir="v") # 열 단위 비교 (열부터 채움) 
+
+# facet_grid
+my_plot <- mpg %>%
+  filter(cyl != 5, drv != "r") %>%
+  ggplot() +
+  geom_point(mapping=aes(x=displ, y=hwy))
+
+my_plot + facet_grid(drv ~ .) # drv별로 facet grid
+my_plot + facet_grid(. ~ cyl) # cyl별로 ""
+my_plot + facet_grid(drv ~ cyl) # drv와 cyl로 
+
+# 4파트로 비슷한 개수로 분리
+air_1 <-airquality %>% mutate(Wind_d = cut_number(Wind,4))
+air_1 %>% ggplot() +
+  geom_point(mapping=aes(x=Solar.R,y=Ozone)) +
+  facet_wrap(~Wind_d)
+
+air_1 %>%
+  ggplot() +
+  geom_point(mapping=aes(x=Solar.R,y=Ozone, color=Wind_d,
+                         size=Wind_d))
